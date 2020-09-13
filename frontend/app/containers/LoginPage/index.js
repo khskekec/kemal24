3af919a15f5c90693de91622970842ffa65b06a1
Selcuk Kekec <skekec@i-doit.com>
2020-09-13 02:03:23 +0200

    BUMP

--- frontend/app/containers/LoginPage/index.js
+++ frontend/app/containers/LoginPage/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import axios from '../../utils/axios';
 import { actions } from '../App/redux';
+import "./Login.scss";
 
 const LoginPage = () => {
   const [username, setUsername] = useState('admin');
@@ -17,27 +18,36 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <div>
-        <input
-          type="text"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          placeholder="Username"
-        />
+    <div className='login-container'>
+      <div className='login-logo font-weight-900 text-size-5 shadow-lg'>
+        <span>K24</span>
       </div>
-      <div>
-        <input
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-      </div>
-      <div>
-        <button className="btn btn-primary" onClick={loginHandler}>
-          Login
-        </button>
+      <div className='form-container'>
+        <div>
+          <input
+            className='form-control form-control-lg shadow-lg'
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+        </div>
+        <br/>
+        <div>
+          <input
+            className='form-control form-control-lg'
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+        <br/>
+        <div>
+          <button className="btn bg-brand btn-block" style={{color: 'white'}} onClick={loginHandler}>
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
