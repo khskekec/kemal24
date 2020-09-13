96ff1d4688a162ad25811b04ef9f5ea6f382ac36
Selcuk Kekec <skekec@i-doit.com>
2020-09-13 02:05:26 +0200

    BUMP

--- backend/src/seeders/20200910083454-default-users.js
+++ backend/src/seeders/20200910083454-default-users.js
@@ -23,6 +23,26 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
+
+    await queryInterface.bulkInsert('Users', [{
+      firstName: 'Selcuk',
+      lastName: 'Kekec',
+      email: 'skekec@kemal24.de',
+      userName: 'skekec',
+      password: crypto.pbkdf2Sync('skekec', process.env.CRYPTO_SALT , 1000, 64, `sha512`).toString(`hex`),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+
+    await queryInterface.bulkInsert('Users', [{
+      firstName: 'Halime',
+      lastName: 'Kekec',
+      email: 'hekec@kemal24.de',
+      userName: 'hkekec',
+      password: crypto.pbkdf2Sync('hkekec', process.env.CRYPTO_SALT , 1000, 64, `sha512`).toString(`hex`),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
