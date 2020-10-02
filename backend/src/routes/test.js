625830786f88f16c85574cba1cc9d1695753e474
Selcuk Kekec <khskekec@gmail.com>
2020-10-02 15:53:44 +0200

    BUMP

--- backend/src/routes/test.js
+++ backend/src/routes/test.js
@@ -2,8 +2,6 @@ import Router from '@koa/router';
 import authentication from "../middelwares/authentication";
 const router = new Router();
 
-router.use(authentication);
-
 router.get('/action', async ctx => {
     const data = await ctx.db().Event.findOne({ include: ctx.db().EventType });
     ctx.body = JSON.stringify(data);
