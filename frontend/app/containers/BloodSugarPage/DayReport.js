5ae6d08334793f40151b97c44ecc0549b44f66c8
Selcuk Kekec <skekec@i-doit.com>
2020-10-30 00:10:06 +0100

    BUMP

--- frontend/app/containers/BloodSugarPage/DayReport.js
+++ frontend/app/containers/BloodSugarPage/DayReport.js
@@ -19,14 +19,13 @@ const DayReport = ({start, end}) => {
 
   const matrix = [];
 
-  console.log(min, max);
   for (var i = min; i < max + 10; i = i + 10) {
     const d = new Array(25).fill('');
     d[0] = i;
 
     for (var j = 0; j < data.length; j++) {
       if (data[j].avg >= i && data[j].avg <= i + 10) {
-        d[parseInt(data[j].point) + 1] = data[j].avg;
+        d[parseInt(data[j].point) + 1] = Math.round(data[j].avg);
       }
     }
 
@@ -47,59 +46,114 @@ const DayReport = ({start, end}) => {
     bolusMatrix[parseInt(e.point) + 1] = e['bolus']
   });
 
-  return <div className='container-fluid'>
+  return <div className='card shadow-lg m-2 table-responsive day-report'>
+    <div className='card-header'>
+      <h3>{moment(start).format('DD.MM.YYYY')}</h3>
+    </div>
+    <div className='card-body table-responsive p-0'>
+      <table className='table table-bordered'>
+        <thead>
+        <tr className='text-center table-dark'>
+          <th scope='col'></th>
+          <th scope='col'>00</th>
+          <th scope='col'>01</th>
+          <th scope='col'>02</th>
+          <th scope='col'>03</th>
+          <th scope='col'>04</th>
+          <th scope='col'>05</th>
+          <th scope='col'>06</th>
+          <th scope='col'>07</th>
+          <th scope='col'>08</th>
+          <th scope='col'>09</th>
+          <th scope='col'>10</th>
+          <th scope='col'>11</th>
+          <th scope='col'>12</th>
+          <th scope='col'>13</th>
+          <th scope='col'>14</th>
+          <th scope='col'>15</th>
+          <th scope='col'>16</th>
+          <th scope='col'>17</th>
+          <th scope='col'>18</th>
+          <th scope='col'>19</th>
+          <th scope='col'>20</th>
+          <th scope='col'>21</th>
+          <th scope='col'>22</th>
+          <th scope='col'>23</th>
+        </tr>
+        </thead>
+        <tbody className='text-center'>
+        {
+          matrix.reverse().map(e => <tr>
+            {e.map(f => <td>{f}</td>)}
+          </tr>)
+        }
+        </tbody>
+        <tfoot className='text-center'>
+        <tr className='table-info'>
+          {bolusMatrix.map(e => <td>{e}</td>)}
+        </tr>
+        <tr className='table-warning'>
+          {correctionMatrix.map(e => <td>{e}</td>)}
+        </tr>
+        </tfoot>
+      </table>
+    </div>
+  </div>;
+  return <div className='container-fluid day-report' >
     <div className='row'>
       <div className='col-12'>
-        <div className='card shadow-lg m-2'>
+        <div className='card shadow-lg m-2 table-responsive'>
           <div className='card-header'>
             <h3>{moment(start).format('DD.MM.YYYY')}</h3>
           </div>
-          <table className='table table-bordered'>
-            <thead>
-            <tr className='text-center'>
-              <th></th>
-              <th>00</th>
-              <th>01</th>
-              <th>02</th>
-              <th>03</th>
-              <th>04</th>
-              <th>05</th>
-              <th>06</th>
-              <th>07</th>
-              <th>08</th>
-              <th>09</th>
-              <th>10</th>
-              <th>11</th>
-              <th>12</th>
-              <th>13</th>
-              <th>14</th>
-              <th>15</th>
-              <th>16</th>
-              <th>17</th>
-              <th>18</th>
-              <th>19</th>
-              <th>20</th>
-              <th>21</th>
-              <th>22</th>
-              <th>23</th>
-            </tr>
-            </thead>
-            <tbody className='text-center'>
-            {
-              matrix.reverse().map(e => <tr>
-                {e.map(f => <td>{f}</td>)}
-              </tr>)
-            }
-            </tbody>
-            <tfoot className='text-center'>
-            <tr>
-              {bolusMatrix.map(e => <td>{e}</td>)}
-            </tr>
-            <tr>
-              {correctionMatrix.map(e => <td>{e}</td>)}
-            </tr>
-            </tfoot>
-          </table>
+          <div className='card-body table-responsive'>
+            <table className='table table-bordered'>
+              <thead>
+              <tr className='text-center'>
+                <th scope='col'></th>
+                <th scope='col'>00</th>
+                <th scope='col'>01</th>
+                <th scope='col'>02</th>
+                <th scope='col'>03</th>
+                <th scope='col'>04</th>
+                <th scope='col'>05</th>
+                <th scope='col'>06</th>
+                <th scope='col'>07</th>
+                <th scope='col'>08</th>
+                <th scope='col'>09</th>
+                <th scope='col'>10</th>
+                <th scope='col'>11</th>
+                <th scope='col'>12</th>
+                <th scope='col'>13</th>
+                <th scope='col'>14</th>
+                <th scope='col'>15</th>
+                <th scope='col'>16</th>
+                <th scope='col'>17</th>
+                <th scope='col'>18</th>
+                <th scope='col'>19</th>
+                <th scope='col'>20</th>
+                <th scope='col'>21</th>
+                <th scope='col'>22</th>
+                <th scope='col'>23</th>
+              </tr>
+              </thead>
+              <tbody className='text-center'>
+              {
+                matrix.reverse().map(e => <tr>
+                  {e.map(f => <td>{f}</td>)}
+                </tr>)
+              }
+              </tbody>
+              <tfoot className='text-center'>
+              <tr>
+                {bolusMatrix.map(e => <td>{e}</td>)}
+              </tr>
+              <tr>
+                {correctionMatrix.map(e => <td>{e}</td>)}
+              </tr>
+              </tfoot>
+            </table>
+          </div>
         </div>
       </div>
     </div>
